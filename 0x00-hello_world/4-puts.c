ae1e73cdb9b4c39aaafb2aeacf69204abaa7affb
Adeyinka500 <oladelesemiu@gmail.com>
2022-09-10 16:22:09 -0700

    coding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put funtion
+*
+* Description: using the main funtion 
+* this program prints "programming is like building multilingua puzzle
+* Return: 0
+*/
+int main(void)
+(
+     puts("\"programming is like building multilingua puzzle");
+     return (0);
+)
