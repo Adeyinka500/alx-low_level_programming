fe65ae0b306c39cf82b8b8f0a16a74ad02146bfe
Adeyinka500 <oladelesemiu@gmail.com>
2022-09-11 09:51:32 -0700

    printf

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,7 +8,7 @@
 
 * Description: using the main function
 
- */this program prints "Programming is positive, zero, or negative
+ this program prints "Programming is positive, zero, or negative
 
 * Return: 0
 
